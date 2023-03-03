f890e1c87910376121727d95064af5d46c797840
Mikolaj Mikolajewski <mikolaj.jewski@gmail.com>
2023-03-03 20:26:16 +0000

    rozwiazanie 1

--- src/main/java/pl/javastart/task/Season.java
+++ src/main/java/pl/javastart/task/Season.java
@@ -1,5 +1,24 @@
 package pl.javastart.task;
 
 public enum Season {
-    //uzupełnij
+    SPRING("Wiosna", new String[]{"marzec", "kwiecień", "maj"}),
+    SUMMER("Lato", new String[]{"czerwiec", "lipiec", "sierpień"}),
+    AUTUMN("Jesień", new String[]{"wrzesień", "październik", "listopad"}),
+    WINTER("Zima", new String[]{"grudzień", "styczeń", "luty" });
+
+    private final String destription;
+    private final String[] months;
+
+    Season(String destription, String[] months) {
+        this.destription = destription;
+        this.months = months;
+    }
+
+    public String getDestription() {
+        return destription;
+    }
+
+    public String[] getMonths() {
+        return months;
+    }
 }
